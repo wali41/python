80ba2c3088882250e3d846e4a867f8b77b0752de
wali41 <samialakho@gmail.com>
2025-07-23 20:35:32 +0500

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,37 @@
+num1 = 45
+
+num2 = 3
+
+print("Addition", num1 + num2)
+
+print("Subtraction", num1 - num2)
+
+#Artithmetic Operators
+
+print("Addition: ", num1 + num2)
+
+print("Subtraction: ", num1 - num2)
+
+print("Multiplication: ", num1 * num2)
+
+print("Division: ", num1 / num2)
+
+print("Modulus: ", num1 % num2)
+
+print("Exponent: ", num1 ** num2)
+
+print("Floor Division: ", num1 // num2)
+
+#Comparison Operators
+
+print("Equal: ", num1 == num2)
+
+print("Not Equal: ", num1 != num2)
+
+print("Greater Than: ", num1 > num2)
+
+print("Less Than: ", num1 < num2)
+
+print("Greater Than or Equal to: ", num1 >= num2)
+
+print("Less Than or Equal to: ", num1 <= num2)
---
+++ stringoperations.py
@@ -0,0 +1,27 @@
+# Operation on Strings
+
+first_name = "Codingal"
+
+second_name = "Education"
+
+full_name = first_name + " " + second_name
+
+name = "Wali" * 5
+
+print(first_name)
+
+print(second_name)
+
+print(full_name)
+
+print(name)
+
+word = "Coding"
+
+print(word[0])
+
+print(word[1])
+
+print(len(word))
+
+print(word[1:3])
