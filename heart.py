60570fd0f088b5af6384eeae7dbb2eb5cc57bdf5
wali41 <samialakho@gmail.com>
2025-07-24 09:15:07 +0500

    Add files via upload

---
+++ heart.py
@@ -0,0 +1,44 @@
+import turtle
+
+sc=turtle.Screen()
+sc.bgcolor("green")
+sc.setup(800,800)
+
+
+heart =turtle.Turtle()
+
+heart.color("pink")
+heart.pensize(20)
+
+heart.left(140)
+heart.forward(113)
+heart.circle(-57, 200)
+heart.left(120)
+heart.circle(-57, 200)
+heart.forward(113)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sc.exitonclick()
+
---
+++ octagon.py
@@ -0,0 +1,36 @@
+import turtle
+
+sc =turtle.Screen()
+sc.bgcolor("blue")
+sc.setup(500,500)
+
+
+octagon = turtle.Turtle()
+
+octagon.color("green")
+octagon.pensize(8)
+
+for i in range (8):
+
+    octagon.forward(100)
+    octagon.left(45)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sc.exitonclick()
