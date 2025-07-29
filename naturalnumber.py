7599b835b52403d3b16089c0f5a212aff044c95b
wali41 <samialakho@gmail.com>
2025-07-29 11:55:37 +0500

    Add files via upload

---
+++ naturalnumber.py
@@ -0,0 +1,10 @@
+number =int(input("Enter the number"))
+
+if (number >  0):
+    print("It is a natural number.")
+
+elif(number < 0):
+    print("It is not a a natural number")
+
+else:
+    print("The number is neutral.")         
