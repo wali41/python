564dbabb4ea3d53fcec111a79900a6f65a7a3e53
wali41 <samialakho@gmail.com>
2025-07-30 21:24:36 +0500

    Add files via upload

---
+++ list.py
@@ -0,0 +1,49 @@
+players = ["Pele", "Lionel Messi", "Cristiano Ronaldo", "Neymar Jr", "Sergio Ramos"]
+
+# Length of List
+
+print('Length of List:', len(players))
+
+# First element of List
+
+print(players[0])
+
+# Last element of List
+
+print('Last element of List:', players[-1])
+
+# Append element to List
+
+players.append('Mango')
+
+print(players)
+
+# Remove element from List
+
+players.remove('Banana')
+
+print(players)
+
+# Sort List
+
+players.sort()
+
+print(players)
+
+# pop element from List
+
+players.pop()
+
+print(players)
+
+# reverse List
+
+players.reverse()
+
+print(players)
+
+# Clear List
+
+players.clear()
+
+print(players)
